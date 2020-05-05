8616bcd3bab8f6e8ebaf1cc26240e6c6784da777
liomj <liomj83@gmail.com>
2020-05-05 22:39:55 +0800

    Update mysql.sql

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -5,7 +5,7 @@
 CREATE TABLE `countdown_events` (
   `id`          INT(11)     NOT NULL  AUTO_INCREMENT,
   `uid`         INT(11)     NOT NULL  DEFAULT 0,
-  `name`        VARCHAR(50) NOT NULL,
+  `name`        VARCHAR(255) NOT NULL,
   `description` MEDIUMTEXT  NOT NULL,
   `enddatetime` TIMESTAMP   NOT NULL  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
