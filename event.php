51b5294ea011c5fe03760a1eb2bb4a09cee0f99d
liomj <liomj83@gmail.com>
2020-05-06 10:21:48 +0800

    Update event.php

--- event.php
+++ event.php
@@ -25,7 +25,7 @@
 use Xmf\Request;
 use XoopsModules\Countdown;
 
-$GLOBALS['xoopsOption']['template_main'] = 'countdown_events.tpl';
+$GLOBALS['xoopsOption']['template_main'] = 'countdown_event.tpl';
 require_once __DIR__ . '/header.php';
 $start = Request::getInt('start', 0);
 // Define Stylesheet
@@ -50,7 +50,7 @@ $eventsArray = $eventsHandler->getAll($criteria);
 
 $id = Request::getInt('id', 0, 'GET');
 
-        //        viewItem();
+        //viewItem();
         $eventsPaginationLimit                   = 1;
         $myid                                    = $id;
         //id
